3c351bc2d83a1f6ccc481cdc60172664082bde15
pierslabs <pedrolosasp@gmail.com>
2023-02-23 08:39:37 +0100

    PA-6:feature/create usePodcastStore.

---
+++ src/hooks/usePodcastStore.js
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from 'react-redux';
+import podcastApi from '../api/podcastApi';
+import { onloadPodcasts } from '../store/podcast/podcastSlice';
+
+const { VITE_ITUNES_TOP_PODCAST_URC } = import.meta.env;
+
+const usePodcatStore = () => {
+  const dispatch = useDispatch();
+  const { podcasts } = useSelector((state) => state.podcast);
+
+  const startOnLoadingPodcast = async () => {
+    try {
+      const { data } = await podcastApi.get(VITE_ITUNES_TOP_PODCAST_URC);
+      dispatch(onloadPodcasts(data.feed.entry));
+    } catch (error) {
+      const errors = { errorTrace: new Error(), errorMessasge: error.message };
+      console.log(errors);
+    }
+  };
+
+  return {
+    //Properties
+    podcasts,
+    //Methods
+    startOnLoadingPodcast,
+  };
+};
+
+export default usePodcatStore;
--- src/store/podcast/podcastSlice.js
+++ src/store/podcast/podcastSlice.js
@@ -19,4 +19,4 @@ export const podcastSlice = createSlice({
 });
 
 // Action creators are generated for each case reducer function
-export const { increment } = podcastSlice.actions;
+export const { onsetActivePodcast, onloadPodcasts } = podcastSlice.actions;
