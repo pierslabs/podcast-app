71ead71366602f1ed88c5d5d0a5d12553f1672a3
pierslabs <pedrolosasp@gmail.com>
2023-02-24 11:13:24 +0100

    PA-15:bugfix/refactor && fixed startOnLoadingDetailPodcast.

--- src/hooks/usePodcastStore.js
+++ src/hooks/usePodcastStore.js
@@ -1,8 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
-import podcastApi from '../api/podcastApi';
+import { podcastApi, podcastApiProxi } from '../api/podcastApi';
 import { onloadPodcasts } from '../store/podcast/podcastSlice';
+import { parse } from 'rss-to-json';
 
-const { VITE_ITUNES_TOP_PODCAST_URC } = import.meta.env;
+const { VITE_ITUNES_TOP_PODCAST_URC, VITE_ITUNES_DETAIL_PODCAST_URC } = import.meta.env;
 
 const usePodcatStore = () => {
   const dispatch = useDispatch();
@@ -20,6 +21,25 @@ const usePodcatStore = () => {
     }
   };
 
+  const startOnLoadingDetailPodcast = async (id) => {
+    try {
+      const { data } = await podcastApiProxi.get(`${VITE_ITUNES_DETAIL_PODCAST_URC}${id}`);
+      const feedUrl = await data.results[0].feedUrl;
+
+      return await parse(feedUrl);
+    } catch (error) {
+      if (error.response.status === 403) {
+        const { data } = await podcastApi.get(`${VITE_ITUNES_DETAIL_PODCAST_URC}${id}`);
+        const feedUrl = await data.results[0].feedUrl;
+
+        return await parse(feedUrl);
+      } else {
+        const errors = { errorTrace: new Error(), errorMessasge: error.message };
+        console.log(errors);
+      }
+    }
+  };
+
   return {
     //Properties
     podcasts,
@@ -30,6 +50,7 @@ const usePodcatStore = () => {
 
     //Methods
     startOnLoadingPodcast,
+    startOnLoadingDetailPodcast,
   };
 };
 
