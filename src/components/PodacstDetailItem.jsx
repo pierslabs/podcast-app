3614294af445ef19a1284f90e1e540585a16a1ff
pierslabs <pedrolosasp@gmail.com>
2023-02-24 11:10:04 +0100

    PA-13:Styled PodacstDetailItem.

--- src/components/PodacstDetailItem.jsx
+++ src/components/PodacstDetailItem.jsx
@@ -7,22 +7,19 @@ const PodacstDetailItem = () => {
 
   if (!activePodcast) return <Navigate to="/" />;
   return (
-    <div className="border p-5 w-1/3 flex flex-col items-center shadow-xl h-2/5">
-      <div className="mb-2">
+    <div className="border p-6 flex flex-col shadow-xl h-2/5 w-1/4 mt-5">
+      <div className="mb-2 p-2">
         <img className="w-64 mx-auto mb-3 rounded" src={activePodcast['im:image'][2].label} alt="Avatar of Writer" />
       </div>
 
-      <div className="mb-2 border-b-2 border-t-2  border-grey-300 p-2">
-        <label></label>
+      <div className="mb-2 border-b-2 border-t-2  border-grey-300 p-3">
         <div className="text-gray-900 font-semibold text mb-2">{activePodcast.title.label}</div>
-        <p className=" text-slate-800">by {activePodcast['im:artist'].label}</p>
+        <p className=" text-slate-700">by {activePodcast['im:artist'].label}</p>
       </div>
 
-      <div className="flex items-center">
-        <div className="text-sm">
-          <p className=" font-semibold text-slate-700">Description</p>
-          <p className="text-gray-600">{activePodcast.summary.label}</p>
-        </div>
+      <div className="border-grey-300  px-3">
+        <div className="text-gray-600 font-semibold text mb-2">Description:</div>
+        <p className="text-gray-600">{activePodcast.summary.label}</p>
       </div>
     </div>
   );
